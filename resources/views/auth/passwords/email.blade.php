5a5e9b45e9d9dae2afeb400ae2a86e44e885ba0a
aftermath089 <vincentmichael089@gmail.com>
2019-07-30 18:12:23 +0800

    add dashboard link in dropdown

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -65,6 +65,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="/home" class="dropdown-item">Dashboard</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
